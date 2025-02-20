b4523b9341582f9834557efea028960c00ed4a6f
MuPinggg07 <champkung1123@gmail.com>
2025-02-20 14:53:11 +0700

    first commit

---
+++ database.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mini_project_db',
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error Connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+module.exports = db;
+    
